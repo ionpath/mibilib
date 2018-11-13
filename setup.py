1d66d76b5b38001c859e0ca39884e054c45374af
Jay <32075205+jay-ionpath@users.noreply.github.com>
2018-11-13 10:44:49 -0800

    Add setup.py file (#7)
    * Add API tutorial.
    
    * Create setup.py file for installing from pip
    
    * Add python_requres
    
    * python_requires>=3.6
    
    * Add packages arg
    
    * Add description in __init__
    
    * Copyright and other setup args

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Setup file for installing dependencies for MIBItracker-Client
+
+Copyright (C) 2018 Ionpath, Inc.  All rights reserved."""
+
+from setuptools import setup
+
+setup(name='mibitracker-client',
+      author='IONpath, Inc.',
+      author_email='mibitracker-support@ionpath.com',
+      version='1.0',
+      url='https://github.com/ionpath/mibitracker-client',
+      description='Python client for IONpath MIBItracker API.',
+      license='GNU General Public License v3.0',
+      python_requires='==3.6.*',
+      install_requires=[
+          'pip==18.0',
+          'requests==2.14.2'
+      ],
+      packages=['mibitracker']
+     )
