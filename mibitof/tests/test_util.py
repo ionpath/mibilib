03edfdaed624b6dd744d249860c5f76ee837fa48
Jay Tarolli <jay@ionpath.com>
2018-11-19 12:01:49 -0800

    Add util

---
+++ mibitof/tests/test_util.py
@@ -0,0 +1,57 @@
+"""Tests for mibiscope.util"""
+
+import unittest
+from mibitof import util
+
+
+class TestUtil(unittest.TestCase):
+
+    def test_natural_sort(self):
+        names = ['Image0.msdf', 'Image0.h5', 'Image10.h5', 'Image2.h5']
+        util.natural_sort(names)
+        expected_names = ['Image0.h5', 'Image0.msdf', 'Image2.h5', 'Image10.h5']
+        self.assertEqual(names, expected_names)
+
+    def test_encode_list(self):
+
+        strings = ['Five', 'birds', 'one', 'stone']
+        bytes_objects = ['Five'.encode('utf-8'), 'birds'.encode('utf-8'),
+                         'one'.encode('utf-8'), 'stone'.encode('utf-8')]
+
+        encoded = util.encode_strings(strings)
+        self.assertListEqual(encoded, bytes_objects)
+
+    def test_decode_list(self):
+
+        strings = ['Five', 'birds', 'one', 'stone']
+        bytes_objects = ['Five'.encode('utf-8'), 'birds'.encode('utf-8'),
+                         'one'.encode('utf-8'), 'stone'.encode('utf-8')]
+
+        decoded = util.decode_strings(bytes_objects)
+        self.assertListEqual(decoded, strings)
+
+    def test_sort_channel_names(self):
+        expected_list = ["30CD", "35 CD", "CD20", "CD45", "dsDNA", "FOXP3",
+                         "HLA DR", "Keratin", "PD-L1", "Vimentin",
+                         "\u03D0-tubulin", "23", "150", "151", "162", "165",
+                         "175"]
+
+        unsorted_list = ["165", "dsDNA", "23", "Keratin", "CD45", "CD20", "162",
+                         "35 CD", "151", "\u03D0-tubulin", "FOXP3", "PD-L1",
+                         "175", "30CD", "Vimentin", "HLA DR", "150"]
+
+        util.sort_channel_names(unsorted_list)
+
+        self.assertListEqual(unsorted_list, expected_list)
+
+
+    def test_format_for_filename(self):
+
+        input_names = ['ChannelWith/Slash', 'ChannelWithoutSlash',
+                       'ChannelWithDouble\\Slash', 'NF-\u03BAB']
+        expected_names = ['ChannelWith-Slash', 'ChannelWithoutSlash',
+                          'ChannelWithDouble-Slash', 'NF-κB']
+
+        formatted_names = [util.format_for_filename(n) for n in input_names]
+
+        self.assertListEqual(formatted_names, expected_names)
---
+++ mibitof/util.py
@@ -0,0 +1,110 @@
+""" Common convenience functions used in mibitof library. """
+
+import re
+import functools
+
+DELIMITERS = {'/', '\\'}
+
+
+def encode_strings(string_list, encoding='utf-8'):
+    """Encodes each string in a list into a bytes object and returns a list
+    of bytes objects. Useful function when writing and reading string lists
+    which are not supported in pytables.
+
+    Args:
+        string_list: A list of strings to encode.
+        encoding: Encoding to use, defaults to UTF-8.
+
+    Returns:
+        A list of bytes objects that have been encoded with the given encoding.
+    """
+    return [s.encode(encoding) for s in string_list]
+
+
+def decode_strings(bytes_objects_list, encoding='utf-8'):
+    """Decodes each bytes object in a list into a string and returns a list
+    of strings. Useful function when writing and reading string lists which are
+    not supported in pytables.
+
+    Args:
+        bytes_objects_list: A list of strings to encode.
+        encoding: Encoding to use, defaults to UTF-8.
+
+    Returns:
+        A list of strings that have been decoded with the given encoding.
+    """
+    return [b.decode(encoding) for b in bytes_objects_list]
+
+
+def natural_sort(l):
+    """Sorts a list of strings in the way that humans expect, in place.
+
+    For example, this would order *'Depth2'* before *'Depth11'*, whereas
+    ``sort`` would place those in the opposite order.
+
+    Args:
+        l: A list of strings.
+    """
+    def _strtoint(s):
+        try:
+            return int(s)
+        except ValueError:
+            return s
+
+    def _alphanum_key(s):
+        """Converts a string into a list of string and number."""
+        return [_strtoint(c) for c in re.split('([0-9]+)', s)]
+    l.sort(key=_alphanum_key)
+
+
+def sort_channel_names(l):
+    """Sorts a list of string in place such that number only string are after
+    all text strings and text strings with numbers are sorted alphabetically.
+
+    ex: ['beta-tubulin', 'CD20', 'CD4', 'CD45', 'CD8', 'dsDNA', 'Keratin',
+        '23', '97', '144', '150']
+
+    Args:
+        l: A list of strings.
+    """
+
+    def compare_items(x, y):
+        """The return value is negative if x < y,
+        zero if x == y and strictly positive if x > y
+        """
+        try:
+            fx = float(x)
+            try:
+                fy = float(y)
+                # x and y are both numbers
+                if fx < fy:
+                    return -1
+                if fx > fy:
+                    return 1
+                return 0
+            except ValueError:
+                # x is a number, y is a non-number
+                return 1
+        except ValueError:
+            try:
+                fy = float(y)
+                # y is a number, x is a non-number
+                return -1
+            except ValueError:
+                # x and y are non-numbers, compare lower case
+                x_lower = x.lower()
+                y_lower = y.lower()
+                if x_lower < y_lower:
+                    return -1
+                if x_lower > y_lower:
+                    return 1
+                return 0
+
+    l.sort(key=functools.cmp_to_key(compare_items))
+
+
+def format_for_filename(label):
+    """Replaces delimiters and utf-8 encodes targets for use as filenames."""
+    for char in set(label).intersection(DELIMITERS):
+        label = label.replace(char, '-')
+    return label
