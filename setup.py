45d4aa3fc139b36f2be6765bcb33469054489f99
Rachel Finck <rachel.finck@gmail.com>
2018-12-15 10:31:58 -0800

    Let requests decide which version of urllib.

--- setup.py
+++ setup.py
@@ -9,14 +9,13 @@ setup(name='mibitracker-client',
       author_email='mibitracker-support@ionpath.com',
       version='1.0',
       url='https://github.com/ionpath/mibitracker-client',
-      description='Python client for IONpath MIBItracker API.',
+      description='Python client and utilities for IONpath MIBItracker',
       license='GNU General Public License v3.0',
       python_requires='==3.6.*',
       install_requires=[
           'pip==18.0',
           'requests==2.20.1',
           'scikit-image==0.13.0',
-          'urllib3==1.22',
       ],
       packages=['mibitracker', 'mibidata']
      )
