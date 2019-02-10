b6da018bf9cabec6d17ea445d58a3432b4bdb9dd
Rachel Finck <rachel.finck@gmail.com>
2019-02-09 17:03:29 -0800

    Update setup.py dependencies to match environment.yml

--- setup.py
+++ setup.py
@@ -9,14 +9,15 @@ setup(name='mibitracker-client',
       author_email='mibitracker-support@ionpath.com',
       version='1.0',
       url='https://github.com/ionpath/mibitracker-client',
-      description='Python client and utilities for IONpath MIBItracker',
+      description='Python utilities for IONpath MIBItracker and MIBItiff data',
       license='GNU General Public License v3.0',
       python_requires='==3.6.*',
       install_requires=[
-          'numpy==1.13.1',
+          'numpy==1.16.0',
+          'pandas==0.21.0',
           'pip==18.0',
           'requests==2.20.1',
-          'scikit-image==0.13.0',
+          'scikit-image==0.14.2',
       ],
       packages=['mibitracker', 'mibidata']
      )
