9062cdda8a4f6d27eb4eff5fa4e8635e8c0e5c60
Rachel Finck <rachel.finck@gmail.com>
2019-03-03 10:22:10 -0800

    Add utilities for working with segmented data.

---
+++ mibidata/segmentation.py
@@ -0,0 +1,158 @@
+"""Utilities for working with segmentation data.
+
+Copyright (C) 2019 Ionpath, Inc.  All rights reserved."""
+
+import numpy as np
+import pandas as pd
+from scipy import ndimage as ndi, stats
+
+from mibidata import mibi_image as mi
+
+
+def extract_cell_dataframe(label_image, image=None, mode='total'):
+    """Creates a dataframe of single-cell statistics from a labeled image.
+
+    Args:
+        label_image: An NxM array where each pixel's nonnegative integer value
+            corresponds to the label of an image region, such as a cell or
+            other segment.
+        image: Optionally, a MibiImage of an NxM field of view. Defaults to
+            None; if not None, the sum or score (depending on the mode) of each
+            channel within each labeled region is returned as column of the
+            dataframe. Otherwise, only the regions' size and area are returned.
+        mode: One of``'total'`` or ``'quadrant'``, defaulting to ``'total'``. If
+            ``'total'`, the ion counts within each labeled region are summed. If
+            ``quadrant``, the geometric mean of each regions's four quadrants
+            is calculated, which favors regions with even spatial distribution.
+            This mode is ignored if an image is not specified.
+
+    Returns:
+        A dataframe indexed by image region's label, and whose columns
+        include the area, centroid, and if included the total or scored
+        counts of the image's channels within each region.
+    """
+    segment_labels = np.unique(label_image)
+    segment_labels = segment_labels[segment_labels > 0]
+
+    columns = ['label', 'area', 'x_centroid', 'y_centroid']
+    if image is not None:
+        columns += list(image.labels)
+
+    rows = []
+    for segment_label in segment_labels:
+        region = label_image == segment_label
+        nonzeros = np.nonzero(region)
+
+        row = [segment_label, len(nonzeros[0]), int(round(nonzeros[1].mean())),
+               int(round(nonzeros[0].mean()))]
+        if image is not None:
+            if mode == 'total':
+                vals = image.data[nonzeros[0], nonzeros[1], :].sum(axis=0)
+            elif mode == 'quadrant':
+                vals = _quadrant_mean(nonzeros, image)
+            else:
+                raise ValueError('"mode" must be either "total" or "quadrant"')
+            row.extend(vals)
+        rows.append(row)
+    return pd.DataFrame(rows, columns=columns).set_index('label')
+
+
+def _quadrant_mean(inds, image):
+    """Gets the geometric mean of a regions's intensities across its quadrants.
+
+    Args:
+        inds: A tuple of 2 arrays of the y- and x- indices of the pixels in a
+            segmented region of an image.
+        image: A MibiImage in which the corresponding pixel indices are located.
+
+    Returns:
+        An array whose length is equal to the number of channels in the image.
+        Each value in the array is the geometric mean of the image's integrated
+        channel intensities over the regions's four quadrants.
+    """
+    y_center, x_center = np.mean(inds, axis=1)
+    vals = image.data[inds]  # has shape (num_pixels_in_cell, num_channels)
+    upper_left = vals[(inds[0] <= y_center) & (inds[1] < x_center)].sum(axis=0)
+    upper_right = vals[(inds[0] < y_center) & (inds[1] >= x_center)].sum(axis=0)
+    lower_left = vals[(inds[0] > y_center) & (inds[1] <= x_center)].sum(axis=0)
+    lower_right = vals[(inds[0] >= y_center) & (inds[1] > x_center)].sum(axis=0)
+    quads = np.stack((upper_left, upper_right, lower_left, lower_right), axis=1)
+    return np.power(np.product(quads, axis=1), 1 / 4)
+
+
+def replace_labeled_pixels(label_image, df, columns=None):
+    """Replaces the pixels within each label with a value from a dataframe.
+
+    Args:
+        label_image: An NxM array where each pixel's nonnegative integer value
+            corresponds to the label of an image region, such as a cell or
+            other segment.
+        df: A dataframe whose index corresponds to the integers in the
+            label_array, and whose column values will replace the labels in the
+            returned image.
+        columns: An optional sequence of which columns from the dataframe to
+            include in the returned image. Defaults to None, which uses all
+            columns in the dataframe.
+
+    Returns:
+        A :class:`mibitof.mibi_image.MibiImage` instance where each channel
+        corresponds to a dataframe column, and the data is a copy of the label
+        image where each pixel has been replaced with the corresponding value
+        from that label's row in the dataframe.
+    """
+    if columns is None:
+        columns = df.columns
+    label_array = np.zeros((label_image.max() + 1, len(columns)),
+                           dtype=label_image.dtype)
+    label_array[df.index, :] = df[columns]
+    columns = [str(i) for i in columns]
+    return mi.MibiImage(label_array[label_image], columns)
+
+
+def expand_objects(label_image, distance):
+    """Expands labeled objects in an image by a given number of pixels.
+
+    Args:
+        label_image: An NxM array where each pixel's nonnegative integer value
+            corresponds to the label of an image region, such as a cell or
+            other segment.
+        distance: The distance (in pixels) to expand each object.
+
+    Returns:
+        A new label array of the expanded objects.
+    """
+    background = label_image == 0
+    distances, (i, j) = ndi.distance_transform_edt(background,
+                                                   return_indices=True)
+    new_labels = label_image.copy()
+
+    # This creates a mask for the pixels we will expand into, and then
+    # sets them to the label of the closet non-background pixel.
+    mask = background & (distances <= distance)
+    new_labels[mask] = label_image[i[mask], j[mask]]
+    return new_labels
+
+
+def filter_by_size(label_image, min_size, max_size):
+    """Removes segments outside of a specified size range.
+
+    Args:
+        label_image: An NxM array where each pixel's nonnegative integer value
+            corresponds to the label of an image region, such as a cell or
+            other segment.
+        min_size: The minimum area in pixels of a segment.
+        max_size: The maximum area in pixels of a segment.
+
+    Returns:
+        A new label image, where segments outside of the size range have been
+        set to zero, and a dataframe of its labels, centroids and area.
+    """
+    df = extract_cell_dataframe(label_image)
+    segment_labels = df.index[(df['area'] >= min_size) &
+                              (df['area'] <= max_size)]
+    new_labels = list(range(1, len(segment_labels) + 1))
+    new_image = replace_labeled_pixels(
+        label_image, pd.DataFrame(new_labels, index=segment_labels))
+    new_df = pd.DataFrame(df.loc[segment_labels, :]).set_index(
+        pd.Index(new_labels, name='label'))
+    return np.squeeze(new_image.data), new_df
---
+++ mibidata/tests/test_segmentation.py
@@ -0,0 +1,180 @@
+"""Tests for mibidata.segmentation
+
+Copyright (C) 2019 Ionpath, Inc.  All rights reserved."""
+
+import unittest
+
+import numpy as np
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+import pandas as pd
+import pandas.util.testing as pdt
+
+from mibidata import mibi_image as mi, segmentation
+
+
+class TestSegmentation(unittest.TestCase):
+
+    def setUp(self):
+        np.random.seed(seed=20160906)
+
+    def test_quadrant_mean(self):
+        data = np.stack((
+            np.array([
+                [3, 2, 4],
+                [1, 1, 3],
+                [0, 0, 1]]),
+            np.array([
+                [0, 0, 1],
+                [0, 0, 0],
+                [0, 0, 2]]),
+        ), axis=2)
+        # assume labels are for cell ID 1, such as with label image:
+        # np.array([
+        #     [0, 1, 1],
+        #     [1, 1, 1],
+        #     [0, 0, 0]
+        # ])
+        inds = ((0, 0, 1, 1, 1), (1, 2, 0, 1, 2))
+        quads_channel_0 = np.power(2 * 4 * (1 + 1) * 3, 1 / 4) # pylint: disable=assignment-from-no-return
+        quads_channel_1 = np.power(0 * 1 * (0 + 0) * 0, 1 / 4) # pylint: disable=assignment-from-no-return
+        expected = np.array((quads_channel_0, quads_channel_1))
+        assert_array_equal(
+            segmentation._quadrant_mean(inds, mi.MibiImage(data, ['1', '2'])),
+            expected
+        )
+
+    def test_extract_cell_dataframe(self):
+        data = np.stack((
+            np.array([
+                [3, 2, 4, 0],
+                [1, 1, 3, 1],
+                [0, 0, 1, 1],
+                [5, 0, 3, 1]]),
+            np.array([
+                [0, 0, 1, 0],
+                [0, 0, 0, 1],
+                [0, 0, 2, 0],
+                [5, 0, 0, 0]]),
+            ), axis=2)
+        cell_labels = np.array([
+            [0, 1, 1, 0],
+            [1, 1, 3, 3],
+            [0, 0, 3, 3],
+            [0, 0, 3, 3]
+        ])
+        image = mi.MibiImage(data, ['1', '2'])
+        labels = [1, 3]
+        areas = [4, 6]
+        x_centroids = [1, 2]
+        y_centroids = [0, 2]
+        first_total = [8, 10]
+        second_total = [1, 3]
+        # Check coords and areas only
+        expected_from_labels = pd.DataFrame(
+            np.array([areas, x_centroids, y_centroids]).T,
+            columns=['area', 'x_centroid', 'y_centroid'],
+            index=pd.Index(labels, name='label'))
+        pdt.assert_frame_equal(
+            segmentation.extract_cell_dataframe(cell_labels),
+            expected_from_labels)
+        # Check mode 'total'
+        expected_from_total = pd.DataFrame(
+            np.array([first_total, second_total]).T,
+            columns=['1', '2'], index=pd.Index(labels, name='label'))
+        pdt.assert_frame_equal(
+            segmentation.extract_cell_dataframe(cell_labels, image),
+            pd.concat((expected_from_labels, expected_from_total), axis=1))
+        # Check mode 'quadrant'
+        quads = []
+        for label in labels:
+            inds = np.nonzero(cell_labels == label)
+            quads.append(segmentation._quadrant_mean(inds, image))
+        expected_from_quadrants = pd.DataFrame(
+            np.array(quads),
+            columns=['1', '2'], index=pd.Index(labels, name='label'))
+        pdt.assert_frame_equal(
+            segmentation.extract_cell_dataframe(
+                cell_labels, image, mode='quadrant'),
+            pd.concat((expected_from_labels, expected_from_quadrants), axis=1))
+
+    def test_replace_labeled_pixels(self):
+        cell_labels = np.array([
+            [0, 1, 1, 0],
+            [1, 1, 3, 3],
+            [0, 0, 3, 3],
+            [0, 0, 3, 3]
+        ])
+        df = pd.DataFrame([
+            [100, 0],
+            [150, 25],
+        ], columns=['dsDNA', 'CD45'], index=pd.Index([1, 3], name='label'))
+        expected_data = np.stack((
+            np.array([
+                [0, 100, 100, 0],
+                [100, 100, 150, 150],
+                [0, 0, 150, 150],
+                [0, 0, 150, 150]
+            ]),
+            np.array([
+                [0, 0, 0, 0],
+                [0, 0, 25, 25],
+                [0, 0, 25, 25],
+                [0, 0, 25, 25]
+            ]),
+        ), axis=2)
+        self.assertEqual(
+            segmentation.replace_labeled_pixels(cell_labels, df),
+            mi.MibiImage(expected_data, ['dsDNA', 'CD45']))
+        self.assertEqual(
+            segmentation.replace_labeled_pixels(cell_labels, df,
+                                                columns=['dsDNA']),
+            mi.MibiImage(expected_data[:, :, [0]], ['dsDNA']))
+
+    def test_filter_by_size(self):
+        cell_labels = np.array([
+            [0, 1, 1, 2],
+            [1, 1, 3, 3],
+            [4, 4, 3, 3],
+            [0, 4, 3, 3]
+        ])
+        expected = np.array([
+            [0, 1, 1, 0],
+            [1, 1, 0, 0],
+            [2, 2, 0, 0],
+            [0, 2, 0, 0]
+        ])
+        df = segmentation.extract_cell_dataframe(expected)
+        filtered_image, filtered_df = segmentation.filter_by_size(
+            cell_labels, 3, 5)
+        assert_array_equal(filtered_image, expected)
+        pdt.assert_frame_equal(filtered_df, df)
+
+    def test_expand_objects(self):
+        labels = np.array([
+            [0, 0, 1, 1, 0],
+            [2, 2, 1, 1, 1],
+            [2, 2, 0, 1, 0],
+            [0, 2, 0, 0, 0],
+            [0, 0, 0, 0, 0]
+        ])
+        expected_1 = np.array([
+            [2, 2, 1, 1, 1],
+            [2, 2, 1, 1, 1],
+            [2, 2, 2, 1, 1],
+            [2, 2, 2, 1, 0],
+            [0, 2, 0, 0, 0]
+        ])
+        expected_2 = np.array([
+            [2, 2, 1, 1, 1],
+            [2, 2, 1, 1, 1],
+            [2, 2, 2, 1, 1],
+            [2, 2, 2, 1, 1],
+            [2, 2, 2, 1, 0]
+        ])
+        assert_array_equal(segmentation.expand_objects(labels, 0), labels)
+        assert_array_equal(segmentation.expand_objects(labels, 1), expected_1)
+        assert_array_equal(segmentation.expand_objects(labels, 2), expected_2)
+
+
+if __name__ == '__main__':
+    unittest.main()
