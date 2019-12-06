6bed526326b6f344b3f81257cb7434bb1f3f9be3
Chityala, Ravi <ravi@ionpath.com>
2019-12-06 13:31:49 -0800

    DPPA-407: Fixed pylint error.

--- check_tag.py
+++ check_tag.py
@@ -1,49 +1,59 @@
+"""
+This file contains functions that checks if tag in git and in
+setup.py are same. If they are not an exception is raised.
+"""
+
 import re
 import subprocess
 
 
-class CheckTag():
-            
-    def check_tag(self):
-        setuppy_version = self.get_tag_in_setuppy("setup.py")
-        tag_version = self.get_latest_tag()
-        print("The version number in setup.py is : ", setuppy_version)
-        print("The version number in git tag is : ", tag_version)
-        return setuppy_version == tag_version
-    
-    def get_latest_tag(self):
-        proc = subprocess.Popen(["git", "describe", "--tag"], 
+def check_tag():
+    """
+    This function checks if tag in git and in setup.py are same and
+    returns a boolean
+    """
+    setuppy_version = get_tag_in_setuppy("setup.py")
+    tag_version = get_latest_tag()
+    print("The version number in setup.py is : ", setuppy_version)
+    print("The version number in git tag is : ", tag_version)
+    return setuppy_version == tag_version
+
+def get_latest_tag():
+    """
+    This function gets the tag from git and formats it as a string
+    for later use.
+    """
+    proc = subprocess.Popen(["git", "describe", "--tag"],\
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = proc.communicate()
-        version = stdout.strip().decode('utf-8')
-        pattern = r"v(\d+).(\d+).(\d+)(.*?)"
-        match = re.match(pattern, version)
-        if(match):
-            major = match.group(1)
-            minor = match.group(2)
-            build = match.group(3)
-            return "v"+".".join((major, minor, build))
-        else:
-            return ""
-            
-    def get_tag_in_setuppy(self, file_path):
-        pattern = r"version='(\d+).(\d+).(\d+)',"
-        with open(file_path) as fo:
-            for line in fo:
-                if line.strip().startswith("version"):
-                    match = re.match(pattern, line.strip())
-                    if(match):
-                        major = match.group(1)
-                        minor = match.group(2)
-                        build = match.group(3)
-                        return "v"+".".join((major, minor, build))
-        return ""
-                
+    stdout, _ = proc.communicate()
+    version = stdout.strip().decode('utf-8')
+    pattern = r"v(\d+).(\d+).(\d+)(.*?)"
+    match = re.match(pattern, version)
+    if match:
+        major = match.group(1)
+        minor = match.group(2)
+        build = match.group(3)
+        return "v"+".".join((major, minor, build))
+    return ""
+
+def get_tag_in_setuppy(file_path):
+    """
+    This function gets the tag from setup.py and formats it as a
+    string for later use.
+    """
+    pattern = r"version='(\d+).(\d+).(\d+)',"
+    with open(file_path) as fo:
+        for line in fo:
+            if line.strip().startswith("version"):
+                match = re.match(pattern, line.strip())
+                if match:
+                    major = match.group(1)
+                    minor = match.group(2)
+                    build = match.group(3)
+                    return "v"+".".join((major, minor, build))
+    return ""
+
 if __name__ == '__main__':
-    ct = CheckTag()
-    if not ct.check_tag():
-        raise Exception("The version number in setup.py and git tag do not match")
-        
-        
-    
-    
+    if not check_tag():
+        raise \
+            Exception("The version number in setup.py and git tag do not match")
