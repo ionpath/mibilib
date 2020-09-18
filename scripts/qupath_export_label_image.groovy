77251247a260f1e23d255da327a73f312e823196
Jay <jay@ionpath.com>
2020-09-18 12:08:28 -0700

    Qupath script for exporting label image

---
+++ scripts/qupath_export_label_image.groovy
@@ -0,0 +1,45 @@
+import qupath.lib.regions.*
+import ij.*
+import java.awt.*
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
+def server = getCurrentImageData().getServer()
+int w = server.getWidth()
+int h = server.getHeight()
+
+int[][] labelArray = new int[h][w]
+
+int index = 1
+for (detection in getDetectionObjects()) {
+    roi = detection.getROI()
+    
+    for (int y = (int)roi.getBoundsY(); y <= (int)(roi.getBoundsY() + roi.getBoundsHeight()); y++){
+        for (int x = (int)roi.getBoundsX(); x <= (int)(roi.getBoundsX() + roi.getBoundsWidth()); x++) {
+            if (roi.contains(x, y)) {
+                labelArray[y][x] = index
+            }
+        }
+    }    
+    index++
+}
+
+def labelImage = new BufferedImage(w, h, BufferedImage.TYPE_USHORT_GRAY)
+
+def g2d = labelImage.createGraphics()
+
+for (int y = 0; y < h; y++) {
+    for (int x = 0; x < w; x++) {
+        labelValue = labelArray[y][x]
+        labelImage.getRaster().setPixel(x, y, labelValue)
+    }
+}
+
+g2d.dispose()
+
+new ImagePlus('Labels', labelImage).show()
+
+def name = getProjectEntry().getImageName()
+def path = buildFilePath(PROJECT_BASE_DIR)
+def fileoutput = new File(path, 'labels.png')
+ImageIO.write(labelImage, 'png', fileoutput)
