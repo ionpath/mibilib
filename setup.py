a754acd34dec4c7c4b083ce465bcf7cd982b1f78
rfinck <rachel@ionpath.com>
2021-03-30 17:10:30 -0700

    DPPA-1411 Add set_fov_id to change both fov_id and folder at same time (#109)
    * Add set_fov_id to change both fov_id and folder at same time
    
    * bump version in setup.py to 1.3.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(name='mibilib',
       author='IONpath, Inc.',
       author_email='support@ionpath.com',
-      version='1.3.1',
+      version='1.3.2',
       url='https://github.com/ionpath/mibilib',
       description='Python utilities for IONpath MIBItracker and MIBItiff data',
       license='GNU General Public License v3.0',
