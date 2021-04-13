ed11bef3bc5843cd39de113b2e9580dab747c268
stanislaw <stanislaw@ionpath.com>
2021-04-13 17:15:17 +0000

    Bump version to 1.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(name='mibilib',
       author='IONpath, Inc.',
       author_email='support@ionpath.com',
-      version='1.3.2',
+      version='1.4.0',
       url='https://github.com/ionpath/mibilib',
       description='Python utilities for IONpath MIBItracker and MIBItiff data',
       license='GNU General Public License v3.0',
