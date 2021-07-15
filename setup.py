24fdf8439d834e85183a28091308ac56bdd22410
murataksoy <32075203+murataksoy@users.noreply.github.com>
2021-07-15 12:09:25 -0700

    Increment mibilib version in setup.py to match the next release (#116)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(name='mibilib',
       author='IONpath, Inc.',
       author_email='support@ionpath.com',
-      version='1.4.0',
+      version='1.4.1',
       url='https://github.com/ionpath/mibilib',
       description='Python utilities for IONpath MIBItracker and MIBItiff data',
       license='GNU General Public License v3.0',
