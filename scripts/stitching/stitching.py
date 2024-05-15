588b8545cca54efbdb80e7f171747f8ffb06d5c5
Mate Nagy <mate.nagy@ionpath.com>
2024-05-14 21:49:15 -0700

    removed need to query mibitracker if not uploading; improved uploaded file name; added margins

--- scripts/stitching/stitching.py
+++ scripts/stitching/stitching.py
@@ -8,12 +8,11 @@ from mibitracker.request_helpers import MibiRequests
 import time
 from mibidata import tiff
 import json
-import glob
 
-MAX_TRIES = 10
+MAX_TRIES = 5
 
 
-def combine_entity_by_name(roi_fov_paths, cols, rows, enforce_square=False):
+def combine_entity_by_name(roi_fov_paths, cols, rows, enforce_square=False, margin=0):
     min_col = np.min(cols)
     min_row = np.min(rows)
     cols=[v-min_col+1 for v in cols]
@@ -23,23 +22,27 @@ def combine_entity_by_name(roi_fov_paths, cols, rows, enforce_square=False):
 
     panel = None
     out_img = None
+    ref_metadata = None
     for fov_i,roi_fov_path in enumerate(roi_fov_paths):
-        print(fov_i+1,roi_fov_path)
-
         fov_img = tiff.read(roi_fov_path)
         panel = fov_img.channels
         fov_img = fov_img.data
+        if ref_metadata is None:
+            ref_metadata = tiff.info(roi_fov_path)
 
         img_shape = list(np.shape(fov_img))
         ch_count = np.min(img_shape)
         shape_2d = img_shape[:-1]
 
         if out_img is None:
-            out_img = np.zeros((h*shape_2d[0], w*shape_2d[1], ch_count), dtype=fov_img.dtype)
+            out_img = np.zeros((h*shape_2d[0]+(h-1)*margin["y"], w*shape_2d[1]+(w-1)*margin["x"], ch_count), 
+                               dtype=fov_img.dtype)
             out_img_shape = list(np.shape(out_img))
 
-        out_img[((rows[fov_i]-1)*shape_2d[0]):(rows[fov_i]*shape_2d[0]), 
-                ((cols[fov_i]-1)*shape_2d[1]):(cols[fov_i]*shape_2d[1]), :] = fov_img[:,:,:]
+        out_img[((rows[fov_i]-1)*shape_2d[0]+(rows[fov_i]-1)*margin["y"]):
+                (rows[fov_i]*shape_2d[0]+(rows[fov_i]-1)*margin["y"]), 
+                ((cols[fov_i]-1)*shape_2d[1]+(cols[fov_i]-1)*margin["x"]):
+                (cols[fov_i]*shape_2d[1]+(cols[fov_i]-1)*margin["x"]), :] = fov_img[:,:,:]
     
     if enforce_square:  # Set to True if there is a need to pad the image to be a square
         if out_img_shape[0] > out_img_shape[1]:
@@ -47,203 +50,167 @@ def combine_entity_by_name(roi_fov_paths, cols, rows, enforce_square=False):
         elif out_img_shape[0] < out_img_shape[1]:
             out_img = np.pad(out_img,((0,out_img_shape[1]-out_img_shape[0]),(0,0),(0,0)))
         
-    return out_img.astype(out_img.dtype, copy=False), panel, int(max(w*shape_2d[1], h*shape_2d[0]))
+    return out_img.astype(out_img.dtype, copy=False), panel, int(max(w*shape_2d[1], h*shape_2d[0])), ref_metadata
 
 
-def stitch_fovs(fovs_folder, out_path, session_dict, upload_to_mibitracker):
-    fov_paths = glob.glob(f'{fovs_folder}/*.tiff')
-    unique_rois = np.unique([os.path.basename(p).split("-")[-1].split(".tiff")[0].split("_")[0] for p in fov_paths])
-    roi_path_groups = dict([(u,[p for p in fov_paths if u in p]) for u in unique_rois])
-    mr = None
-    for t in range(MAX_TRIES):
-        try:
-            mr = MibiRequests(**session_dict)
-            break
-        except:
-            if t < MAX_TRIES-1:
-                time.sleep(0.50)
-            else:
-                mr = MibiRequests(**session_dict)
-
-    for roi,roi_fov_paths in roi_path_groups.items():
-        print(roi)
-
-        cols = []
-        rows = []
-        for fov_path in roi_fov_paths:
-            fov_name = os.path.basename(fov_path).split("-")[-1].split(".tiff")[0]
-            c, r = fov_name.split("_")[1:]
-            cols.append(int(c[1:].lstrip("0")))
-            rows.append(int(r[1:].lstrip("0")))
-
-        out_img, panel, max_dim = combine_entity_by_name(roi_fov_paths, cols, rows, enforce_square=False)
-        out_img = out_img.astype(np.uint8, copy=False)
-
-        um_min_x, um_min_y = 999999999, 999999999
-        for fov_path in roi_fov_paths:
-            
-            json_file = os.path.dirname(fov_path)+"/"+"-".join(os.path.basename(fov_path).split("-")[:2])+"-scan-1.json"
-            with open(json_file) as f:
-                bin_json = json.load(f)
-                coord = bin_json["coordinates"]
+def stitch_fovs(run_folder, out_folder, session_dict, upload_to_mibitracker, fov_margin_size):
+    run = os.path.basename(run_folder)
+    run_json_file = os.path.join(run_folder,run)+".json"
+    with open(run_json_file) as rf:
+        run_json = json.load(rf)
+        for r in run_json["rois"]:
+            if r["standardTarget"] in ["Auto Gain"]:
+                continue
+            roi = r["name"]
+            px_per_u = r["frameSizePixels"]["width"] / r["fovSizeMicrons"]
+            margin = fov_margin_size
+            if margin is None:
+                margin = {
+                    "x":int(np.round(r["xMargin"]*px_per_u)),
+                    "y":int(np.round(r["yMargin"]*px_per_u))
+                }
+            elif type(margin) != dict:
+                if type(margin) != list:
+                    margin = {"x":margin, "y":margin}
+                else:
+                    margin = {"x":margin[0], "y":margin[1]}
+            cols, rows = [], []
+            um_min_x, um_min_y = 999999999, 999999999
+            roi_fov_paths=[]
+            for f in r["fovs"]:
+                fov_name = f["name"]
+                cols.append(f["gridPosition"]["x"]+1)
+                rows.append(f["gridPosition"]["y"]+1)
+                coord = f["centerPointMicrons"]
                 if coord["x"] < um_min_x:
                     um_min_x = coord["x"]
                 if coord["y"] < um_min_y:
                     um_min_y = coord["y"]
-
-        run = os.path.basename(os.path.dirname(roi_fov_paths[0]))
-        fov = "FOV"+os.path.basename(roi_fov_paths[0]).split("-")[1]
-        print(run, fov)
+                roi_fov_paths.append(os.path.join(run_folder,"fov"+"-"+str(f["runOrder"]).zfill(2)+"-"+fov_name)+".tiff")
         
-        for t in range(MAX_TRIES):
-            try:
-                try:
-                    ref_image_id = mr.image_id(run, fov)
-                except:
-                    ref_image_id = mr.image_id(run, fov.replace("V0", "V"))
-                break
-            except:
-                if t < MAX_TRIES-1:
-                    time.sleep(0.50)
-                else:
-                    try:
-                        ref_image_id = mr.image_id(run, fov)
-                    except:
-                        ref_image_id = mr.image_id(run, fov.replace("V0", "V"))
-        
-        for t in range(MAX_TRIES):
-            try:
-                ref_json = mr.get('images/{}/'.format(ref_image_id)).json()
-                break
-            except:
-                if t < MAX_TRIES-1:
-                    time.sleep(0.50)
-                else:
-                    ref_json = mr.get('images/{}/'.format(ref_image_id)).json()
+            print(run, roi, f"{len(roi_fov_paths)} FOVs")
+            out_img, panel, max_dim, ref_metadata = combine_entity_by_name(roi_fov_paths, cols, rows, 
+                                                                        enforce_square=False, margin=margin)
+            out_img = out_img.astype(np.uint8, copy=False)
 
-        sample_id = run+"__"+roi
-        f_split = ref_json['folder'].split('/')
-        f_split[0] = sample_id
-        folder_name = '/'.join(f_split[:3])
+            metadata = ref_metadata.copy()
+            metadata["coordinates"] = (um_min_x, um_min_y)
+            metadata["size"] = max_dim/px_per_u
+            metadata["fov_name"] = roi
 
-        px_per_u = ref_json["frame"]/ref_json["fov_size"]
+            out_mibi_tiff = mi.MibiImage(
+                out_img, panel, datetime_format='%Y-%m-%d', **metadata)
+            
+            f_split = out_mibi_tiff.folder.split('/')
+            f_split[0] = roi
+            out_mibi_tiff.set_fov_id(f_split[0], '/'.join(f_split))
 
-        metadata = {
-            'run': '{}'.format(ref_json['run']['label']),
-            'date': ref_json['run']['run_date'],
-            'coordinates': (um_min_x, um_min_y),
-            'size': max_dim/px_per_u,  # issue: assumption that w==h doesn't hold
-            'slide': ref_json['section']['slide']['id'],
-            'fov_name': sample_id,
-            'frame': max_dim,  # issue: assumption that w==h doesn't hold
-            'folder': folder_name,
-            'fov_id': f_split[0],
-            # assumption that all fovs are same
-            'dwell': ref_json['dwell_time'],
-            'scans': ','.join([str(d) for d in range(ref_json['depths'])]),
-            # assumption that all fovs are same
-            'aperture': ref_json['run']['aperture']['label'],
-            'instrument': ref_json['run']['instrument']['name'],
-            'tissue': ref_json['formatted_tissue'],
-            'panel': ref_json['section']['panel']['name'],
-            'version': 'alpha',
-            # assumption that all fovs are same
-            'mass_offset': ref_json['mass_offset'],
-            # assumption that all fovs are same
-            'mass_gain': ref_json['mass_gain'],
-            # assumption that all fovs are same
-            'time_resolution': ref_json['time_bin'],
-            'filename': '{}'.format(ref_json['run']['name'])
-        }
+            out_path = os.path.join(out_folder,roi+".tiff")
+            tiff.write(out_path, out_mibi_tiff, dtype=np.float32)
+            print(f"Stitched MIBItiff saved to {out_path}.")
 
-        out_mibi_tiff = mi.MibiImage(
-            out_img, panel, datetime_format='%Y-%m-%d', **metadata)
-
-        f_split = out_mibi_tiff.folder.split('/')
-        f_split[0] = sample_id
-        out_mibi_tiff.set_fov_id(f_split[0], '/'.join(f_split))
-
-        tiff.write(out_path, out_mibi_tiff, dtype=np.float32)
-        print(f"Stitched MIBItiff saved to {out_path}.")
-
-        if upload_to_mibitracker:
-            for t in range(MAX_TRIES):
-                try:
-                    exists = mr.get(
-                        '/images/', params={'run__label': ref_json['run']['label'], 'number': f_split[0]}).json()
-                    break
-                except:
-                    if t < MAX_TRIES-1:
-                        time.sleep(0.50)
-                    else:
-                        exists = mr.get(
-                            '/images/', params={'run__label': ref_json['run']['label'], 'number': f_split[0]}).json()
-
-            full_id = exists['results'][0]['id'] if exists['count'] else None
-
-            if not full_id:
-                new_im_metadata = {}
-                new_im_metadata['run'] = ref_json['run']['id']
-                new_im_metadata['point'] = sample_id
-                new_im_metadata['number'] = out_mibi_tiff.fov_id
-                new_im_metadata['folder'] = out_mibi_tiff.folder
-                new_im_metadata['fov_size'] = max_dim/px_per_u
-                # assumption that all fovs are same
-                new_im_metadata['dwell_time'] = ref_json['dwell_time']
-                # assumption that all fovs are same
-                new_im_metadata['depths'] = ref_json['depths']
-                new_im_metadata['frame'] = max_dim
-                # assumption that all fovs are same
-                new_im_metadata['time_bin'] = ref_json['time_bin']
-                # assumption that all fovs are same
-                new_im_metadata['mass_gain'] = ref_json['mass_gain']
-                # assumption that all fovs are same
-                new_im_metadata['mass_offset'] = ref_json['mass_offset']
-                new_im_metadata['x_coord'] = int(np.round(um_min_x))
-                new_im_metadata['y_coord'] = int(np.round(um_min_y))
-                new_im_metadata['tissue'] = \
-                    ref_json['tissue'] and ref_json['tissue']['id']
-                new_im_metadata['section'] = ref_json['section']['id']
-                # assumption that all fovs are same
-                new_im_metadata['aperture'] = ref_json['aperture']['id']
-                # assumption that all fovs are same
-                new_im_metadata['imaging_preset'] = ref_json['imaging_preset']
-                # assumption that all fovs are same
-                new_im_metadata['lens1_voltage'] = ref_json['lens1_voltage']
-
+            if upload_to_mibitracker:
+                mr = None
                 for t in range(MAX_TRIES):
                     try:
-                        _ = mr.post('/images/', json=new_im_metadata)
+                        mr = MibiRequests(**session_dict)
                         break
                     except:
                         if t < MAX_TRIES-1:
                             time.sleep(0.50)
                         else:
-                            _ = mr.post('/images/', json=new_im_metadata)
+                            mr = MibiRequests(**session_dict)
 
-            for t in range(MAX_TRIES):
-                try:
-                    mr.upload_mibitiff(out_path, run_id=ref_json['run']['id'])
-                    break
-                except:
-                    if t < MAX_TRIES-1:
-                        time.sleep(0.50)
-                    else:
-                        mr.upload_mibitiff(out_path, run_id=ref_json['run']['id'])
-            
-            print(f"Stitched MIBItiff, {out_path} uploaded to MIBItracker.")
+                for t in range(MAX_TRIES):
+                    try:
+                        exists = mr.get(
+                            '/images/', params={'run__label': run, 'number': f_split[0]}).json()
+                        break
+                    except:
+                        if t < MAX_TRIES-1:
+                            time.sleep(0.50)
+                        else:
+                            exists = mr.get(
+                                '/images/', params={'run__label': run, 'number': f_split[0]}).json()
+
+                full_id = exists['results'][0]['id'] if exists['count'] else None
+
+                for t in range(MAX_TRIES):
+                    try:
+                        run_id = mr.get('/runs/', params={'name': run}).json()['results'][0]['id']
+                        break
+                    except:
+                        if t < MAX_TRIES-1:
+                            time.sleep(0.50)
+                        else:
+                            run_id = mr.get('/runs/', params={'name': run}).json()['results'][0]['id']
+
+                new_im_metadata = {}
+                new_im_metadata['run'] = run_id
+                new_im_metadata['point'] = roi
+                new_im_metadata['number'] = out_mibi_tiff.fov_id
+                new_im_metadata['folder'] = out_mibi_tiff.folder
+                new_im_metadata['fov_size'] = max_dim/px_per_u
+                new_im_metadata['dwell_time'] = metadata['dwell']
+                new_im_metadata['depths'] = metadata['scans']
+                new_im_metadata['frame'] = max_dim
+                new_im_metadata['time_bin'] = metadata['time_resolution']
+                new_im_metadata['mass_gain'] = metadata['mass_gain']
+                new_im_metadata['mass_offset'] = metadata['mass_offset']
+                new_im_metadata['x_coord'] = int(np.round(um_min_x))
+                new_im_metadata['y_coord'] = int(np.round(um_min_y))
+                new_im_metadata['tissue'] = metadata['raw_description']['fov']['section']['tissue'] \
+                    and metadata['raw_description']['fov']['section']['tissue']['id']
+                new_im_metadata['section'] = metadata['raw_description']['fov']['section']['id']
+                new_im_metadata['aperture'] = metadata['aperture']
+                new_im_metadata['imaging_preset'] = metadata['imaging_preset']
+                new_im_metadata['lens1_voltage'] = metadata['lens1_voltage']
+
+                if not full_id:
+                    for t in range(MAX_TRIES):
+                        try:
+                            _ = mr.post('/images/', json=new_im_metadata)
+                            break
+                        except:
+                            if t < MAX_TRIES-1:
+                                time.sleep(0.50)
+                            else:
+                                _ = mr.post('/images/', json=new_im_metadata)
+                else:
+                    for t in range(MAX_TRIES):
+                        try:
+                            _ = mr.put(f'/images/{full_id}', json=new_im_metadata)
+                            break
+                        except:
+                            if t < MAX_TRIES-1:
+                                time.sleep(0.50)
+                            else:
+                                _ = mr.put(f'/images/{full_id}', json=new_im_metadata)
+
+                for t in range(MAX_TRIES):
+                    try:
+                        mr.upload_mibitiff(out_path, run_id=run_id)
+                        break
+                    except:
+                        if t < MAX_TRIES-1:
+                            time.sleep(0.50)
+                        else:
+                            mr.upload_mibitiff(out_path, run_id=run_id)
+                
+                print(f"Stitched MIBItiff, {out_path}, uploaded to MIBItracker.")
                         
 
 if __name__ == "__main__":
 
-    fovs_folder = ""  # Local folder path with the original Run name and contents
-    out_path = ""  # Local file path to save the stitched MIBItiff into
-    session_dict = {
+    run_folder = ""  # Local folder path with the original Run name and contents
+    out_folder = ""  # Local folder path to save the stitched MIBItiffs into
+    upload_to_mibitracker = True  # Set to True to upload the resulting stitched MIBItiff to MIBItracker
+    session_dict = {  # Only needed if uploading to MIBItracker
         "url": "",  # MIBItracker backend URL (ex: https://mibitracker.api.ionpath.com/)
         "email": "",  # MIBItracker user name
         "password": ""  # MIBItrackefr user password
     }
-    upload_to_mibitracker = False  # Set to True to upload the resulting stitched MIBItiff to MIBItracker 
+    fov_margin_size = None  # Sets the margin size in pixels when defined (int, list [x,y], or dict {x:0,y:0}); Leave as None to use as defined during imaging Run
 
-    stitch_fovs(fovs_folder, out_path, session_dict, upload_to_mibitracker)
+    stitch_fovs(run_folder, out_folder, session_dict, upload_to_mibitracker, fov_margin_size)
 
